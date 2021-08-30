e3b35f5ae01c22779974311ee35e042403ded1e5
Jehan Tremback <hi@jehan.email>
2021-08-30 14:05:13 -0700

    rust compiles

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,44 @@
 mod utils;
 
 use crate::utils::*;
+use async_std::task::block_on;
 use clarity::{constants::ZERO_ADDRESS, Address as EthAddress};
 use cosmos_gravity::send::send_to_eth;
+use deep_space::coin::Coin;
+use deep_space::Address as CosmosAddress;
+use deep_space::Contact;
+use std::{env, time::Duration};
 
 fn main() {
+    let denom: String = "footoken".into();
     let vitaliks_eth_address =
         EthAddress::parse_and_validate("0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B").unwrap();
 
     let keys = get_keys();
 
-    send_to_eth(
+    let contact = Contact::new(
+        "http://localhost:9090".into(),
+        Duration::from_secs(30),
+        CosmosAddress::DEFAULT_PREFIX,
+    )
+    .unwrap();
+
+    block_on(send_to_eth(
         keys[0].validator_key,
         vitaliks_eth_address,
-        amount,
-        bridge_fee,
-        fee,
-        contact,
-    );
+        Coin {
+            denom: denom.clone(),
+            amount: 1_000_000u64.into(),
+        },
+        Coin {
+            denom: denom.clone(),
+            amount: 1u64.into(),
+        },
+        Coin {
+            denom: denom.clone(),
+            amount: 1u64.into(),
+        },
+        &contact,
+    ))
+    .unwrap();
 }
