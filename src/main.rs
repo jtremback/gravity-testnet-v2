0e6c586077f451e5659f79fe1695fa8cba47d602
Jehan Tremback <hi@jehan.email>
2021-08-30 11:31:12 -0700

    start on piecing together utility function to send to eth

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+mod utils;
+
+use crate::utils::*;
+use clarity::{constants::ZERO_ADDRESS, Address as EthAddress};
+use cosmos_gravity::send::send_to_eth;
+
 fn main() {
-    println!("Hello, world!");
+    let vitaliks_eth_address =
+        EthAddress::parse_and_validate("0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B").unwrap();
+
+    let keys = get_keys();
+
+    send_to_eth(
+        keys[0].validator_key,
+        vitaliks_eth_address,
+        amount,
+        bridge_fee,
+        fee,
+        contact,
+    );
 }
